b3aae67935c5069377069c721679eab63600871b
Chiciuc Nicușor <nicuchiciuc@gmail.com>
2017-09-25 23:39:12 +0300

    Removed unused files and configured webpack to deploy everything including index.html

--- src/script.js
+++ src/script.js
@@ -1,168 +1,169 @@
-import models from './models'
-import {triangleGrid, getLineXYatPercent, sin60, cos60} from './utils'
-import CubeDrawer from './cubeDrawer'
-import CanvasPivot from './canvasPivot'
-import {DrawMethodSingle, DrawMethodFull} from './drawMethods'
+import "../index.html";
 
+import dat from "dat.gui/build/dat.gui.js";
+import models from "./models";
+import { triangleGrid, getLineXYatPercent, sin60, cos60 } from "./utils";
+import CubeDrawer from "./cubeDrawer";
+import CanvasPivot from "./canvasPivot";
+import { DrawMethodSingle, DrawMethodFull } from "./drawMethods";
 
 function ready(fn) {
-	if (document.readyState != 'loading'){
-		fn();
-	} else {
-		document.addEventListener('DOMContentLoaded', fn);
-	}
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
 }
 
-ready(function () {
+ready(function() {
+  const setupObj = {
+    cubeWidth: 100,
+    cubeDistance: 0.67,
 
-	const setupObj = {
-		cubeWidth: 100,
-		cubeDistance: 0.67,
+    color1: "#ff6347",
+    color2: "#008080",
+    color3: "#ffd500",
 
-		color1: '#ff6347',
-		color2: '#008080',
-		color3: '#ffd500',
+    alpha: 0.999,
+    drawBorder: true,
 
-		alpha: 0.999,
-		drawBorder: true,
+    run: true,
 
-		run: true,
+    model: "standard",
 
-		model: 'standard',
+    speed: 10,
 
-		speed: 10,
+    time: 0.001,
+    drawNumbers: false,
 
+    renderMethod: "full",
 
-		time: 0.001,
-		drawNumbers: false,
+    compositeMode: "source-atop"
+  };
 
-		renderMethod: 'full',
+  const compositeTypes = [
+    "source-over",
+    "source-atop",
+    "destination-out",
+    "-----",
+    "source-in",
+    "source-out",
+    "destination-over",
+    "destination-in",
+    "destination-atop",
+    "lighter",
+    "copy",
+    "xor",
+    "multiply",
+    "screen",
+    "overlay",
+    "darken",
+    "lighten",
+    "color-dodge",
 
-		compositeMode: 'source-atop',
-	}
+    "difference",
+    "exclusion"
+  ];
 
-	const compositeTypes = [
-		'source-over',
-		'source-atop',
-		'destination-out',
-		'-----',
-		'source-in',
-		'source-out',
-		'destination-over',
-		'destination-in',
-		'destination-atop',
-		'lighter',
-		'copy',
-		'xor',
-		'multiply',
-		'screen',
-		'overlay',
-		'darken',
-		'lighten',
-		'color-dodge',
+  function initDatGui() {
+    const gui = new dat.GUI();
+    gui.add(setupObj, "cubeWidth", 20, 300).onChange(safeRedraw);
+    gui.add(setupObj, "cubeDistance", 0.5, 1.0).onChange(safeRedraw);
+    gui.add(setupObj, "speed", -50, 50);
+    gui.add(setupObj, "alpha", 0.0, 1.0).onChange(safeRedraw);
+    gui.add(setupObj, "drawBorder").onChange(safeRedraw);
+    gui.add(setupObj, "run").onChange(draw);
+    gui.addColor(setupObj, "color1").onChange(safeRedraw);
+    gui.addColor(setupObj, "color2").onChange(safeRedraw);
+    gui.addColor(setupObj, "color3").onChange(safeRedraw);
+    gui.add(setupObj, "model", Object.keys(models)).onChange(safeRedraw);
 
-		'difference',
-		'exclusion',
-		
-	]
+    const secretFolder = gui.addFolder("secret");
+    secretFolder
+      .add(setupObj, "time", 0.0, 1.0)
+      .listen()
+      .onChange(safeRedraw);
+    secretFolder.add(setupObj, "drawNumbers").onChange(safeRedraw);
+    secretFolder
+      .add(setupObj, "compositeMode", compositeTypes)
+      .onChange(safeRedraw);
+    secretFolder
+      .add(setupObj, "renderMethod", ["single", "full"])
+      .onChange(safeRedraw);
+  }
 
-	function initDatGui () {
-		const gui = new dat.GUI();
-		gui.add(setupObj, 'cubeWidth', 20, 300).onChange(safeRedraw)
-		gui.add(setupObj, 'cubeDistance', 0.5, 1.0).onChange(safeRedraw)
-		gui.add(setupObj, 'speed', -50, 50)
-		gui.add(setupObj, 'alpha', 0.0, 1.0).onChange(safeRedraw)
-		gui.add(setupObj, 'drawBorder').onChange(safeRedraw)
-		gui.add(setupObj, 'run').onChange(draw)
-		gui.addColor(setupObj, 'color1').onChange(safeRedraw)
-		gui.addColor(setupObj, 'color2').onChange(safeRedraw)
-		gui.addColor(setupObj, 'color3').onChange(safeRedraw)
-		gui.add(setupObj, 'model', Object.keys(models)).onChange(safeRedraw)
+  initDatGui();
 
-		const secretFolder = gui.addFolder('secret')
-		secretFolder.add(setupObj, 'time', 0.0, 1.0).listen().onChange(safeRedraw)
-		secretFolder.add(setupObj, 'drawNumbers').onChange(safeRedraw)
-		secretFolder.add(setupObj, 'compositeMode', compositeTypes).onChange(safeRedraw)
-		secretFolder.add(setupObj, 'renderMethod', ['single', 'full']).onChange(safeRedraw)
-	}
+  const canvas = document.getElementById("canvas");
 
-	initDatGui()
+  // For drawing the standard cubes
+  const cubeDrawer = new CubeDrawer(setupObj.cubeWidth);
+  cubeDrawer.draw(setupObj);
+  // document.body.appendChild(cubeDrawer.canvas)
 
-	const canvas = document.getElementById('canvas')
+  // For draggin the canvas
+  const canvasPivot = new CanvasPivot(canvas, safeRedraw, { x: 120, y: 250 });
 
-	// For drawing the standard cubes
-	const cubeDrawer = new CubeDrawer(setupObj.cubeWidth)
-	cubeDrawer.draw(setupObj)
-	// document.body.appendChild(cubeDrawer.canvas)
+  // 2 drawing methods
+  const drawMethSingle = new DrawMethodSingle();
+  const drawMethFull = new DrawMethodFull(canvas.width, canvas.height);
 
-	// For draggin the canvas
-	const canvasPivot = new CanvasPivot(canvas, safeRedraw, {x:120, y:250})
+  // Start animation
+  window.requestAnimationFrame(draw);
 
-	// 2 drawing methods
-	const drawMethSingle = new DrawMethodSingle()
-	const drawMethFull = new DrawMethodFull(canvas.width, canvas.height)
+  function safeRedraw() {
+    cubeDrawer.draw(setupObj);
+    if (!setupObj.run) draw();
+  }
 
-	// Start animation
-	window.requestAnimationFrame(draw)
+  function getCurrentPoints() {
+    const currentModel = models[setupObj.model];
 
-	function safeRedraw () {
-		cubeDrawer.draw(setupObj)
-		if (!setupObj.run)
-			draw()
-	}
+    // From triangle grid to canvas grid
+    const setPoints = currentModel
+      .map(triangleGrid)
+      .map(fn => fn(setupObj.cubeDistance * setupObj.cubeWidth));
 
-	function getCurrentPoints () {
-		const currentModel = models[setupObj.model]
+    // Movement percentage
+    const floatTimePoints = setPoints.map((pnt, ind, arr) =>
+      getLineXYatPercent(pnt, arr[(ind + 1) % arr.length], setupObj.time)
+    );
 
-		// From triangle grid to canvas grid
-		const setPoints = currentModel.map(triangleGrid).map(fn => fn(setupObj.cubeDistance * setupObj.cubeWidth))
+    // Add displacement
+    const timePoints = floatTimePoints.map(({ x, y }) => ({
+      x: canvasPivot.pivot.x + x,
+      y: canvasPivot.pivot.y + y
+    }));
 
-		// Movement percentage
-		const floatTimePoints = setPoints.map((pnt, ind, arr) =>
-			getLineXYatPercent(pnt, arr[(ind + 1) % arr.length],
-				setupObj.time)
-		)
+    return timePoints;
+  }
 
-		// Add displacement
-		const timePoints = floatTimePoints.map(({x, y}) => ({
-			x: canvasPivot.pivot.x + x,
-			y: canvasPivot.pivot.y + y
-		}))
+  function draw() {
+    const ctx = canvas.getContext("2d");
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		return timePoints
-	}
+    const timePoints = getCurrentPoints();
 
-	function draw () {
-		const ctx = canvas.getContext('2d')
-		ctx.clearRect(0, 0, canvas.width, canvas.height)
+    const toPass = {
+      destinationContext: ctx,
+      sourceCanvas: cubeDrawer.canvas,
+      points: timePoints,
+      modelsNow: models[setupObj.model],
+      width: setupObj.cubeWidth,
+      compositeMode: setupObj.compositeMode,
+      drawNumbers: setupObj.drawNumbers
+    };
 
-		const timePoints = getCurrentPoints()
+    if (setupObj.renderMethod == "single") drawMethSingle.draw(toPass);
+    else drawMethFull.draw(toPass);
 
-		const toPass = {
-			destinationContext: ctx,
-			sourceCanvas: cubeDrawer.canvas,
-			points: timePoints,
-			modelsNow: models[setupObj.model],
-			width: setupObj.cubeWidth,
-			compositeMode: setupObj.compositeMode,
-			drawNumbers: setupObj.drawNumbers}
+    if (!setupObj.run) return;
 
-		if (setupObj.renderMethod == 'single')
-			drawMethSingle.draw(toPass)
-		else
-			drawMethFull.draw(toPass)
+    setupObj.time += 0.001 * setupObj.speed;
 
+    if (setupObj.time >= 1) setupObj.time -= 1;
+    if (setupObj.time < 0) setupObj.time += 1;
 
-		if (!setupObj.run)
-			return
-
-		setupObj.time += 0.001 * setupObj.speed
-
-		if (setupObj.time >= 1)
-			setupObj.time -= 1
-		if (setupObj.time < 0)
-			setupObj.time += 1
-
-		window.requestAnimationFrame(draw)
-	}
-})
+    window.requestAnimationFrame(draw);
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,36 @@
-var path = require('path');
+var path = require("path");
 
 const config = {
-  entry: ['babel-polyfill', './src/script.js'],
+  entry: ["babel-polyfill", "./src/script.js"],
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'dist.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "dist.js"
   },
   module: {
-    loaders: [{
-      test: /.js?$/,
-      loader: 'babel-loader',
-      exclude: /node_modules/,
-      // include: [
-      //   path.resolve(__dirname, 'src')
-      // ],
-      query: {
-        presets: ['es2015']
+    loaders: [
+      {
+        test: /.js?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        // include: [
+        //   path.resolve(__dirname, 'src')
+        // ],
+        query: {
+          presets: ["es2015"]
+        }
+      },
+      {
+        test: /\.(html|jpe?g|gif|png|svg|woff|ttf|wav|mp3)$/,
+        exclude: /node_modules/,
+        loader: "file-loader",
+        options: {
+          name: "[path][name].[ext]"
+        }
       }
-    }]
+    ]
   },
 
-
-  devServer: {inline: true},
-
+  devServer: { inline: true }
 };
 
 module.exports = config;
